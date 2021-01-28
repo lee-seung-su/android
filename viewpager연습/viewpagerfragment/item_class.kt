04e4edcf9d64590ed31092eb9a97b46807ba1a17
lee-seung-su <52225690+lee-seung-su@users.noreply.github.com>
2021-01-28 17:03:33 +0900

    Delete viewpager연습 directory

--- viewpager연습/viewpagerfragment/item_class.kt
+++
@@ -1,4 +0,0 @@
-package com.example.viewpagerfragment
-
-class item_class(var image : Int, var name:String, var content : String, var draw : Boolean = false )
-class instagram_item_class(var profile_image : Int?, var name : String, var main_image : Int?)
